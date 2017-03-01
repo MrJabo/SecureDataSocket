9d84aa7d1ddf22e5706bb80976c5aef5a22e1e3b
Tobias Schuelke <tobys.briese@yahoo.de>
2017-03-01 19:03:42 +0100

    fixed: ip address always local host

--- src/main/java/com/cryptolib/SecureDataSocket.java
+++ src/main/java/com/cryptolib/SecureDataSocket.java
@@ -5,9 +5,13 @@ import com.cryptolib.CryptoSocketInterface.Channel;
 import com.cryptolib.CryptoSocketInterface.ChannelType;
 import java.io.IOException;
 import java.io.Serializable;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
 import java.net.SocketAddress;
 import java.net.Inet4Address;
 import java.net.UnknownHostException;
+import java.util.Collections;
+import java.util.List;
 
 public class SecureDataSocket {
 	
@@ -75,7 +79,7 @@ public class SecureDataSocket {
 	 * */
 	public String prepareServerWithClientCamera() throws IOException {
 		this.socket = new FDESocket(new Channel(ChannelType.MANUAL, "::"));
-		return Inet4Address.getLocalHost().getHostAddress()+":"+this.port+":"+this.socket.createSharedSecret();
+		return getIPAddress(true) + ":" + this.port + ":" + this.socket.createSharedSecret();
 	}
 
 
@@ -146,5 +150,39 @@ public class SecureDataSocket {
 
 	public void close() {
 		this.socket.close();
+	}
+
+	/**
+	 * from: http://stackoverflow.com/a/13007325
+	 *
+	 * Get IP address from first non-localhost interface
+	 * @param useIPv4  true=return ipv4, false=return ipv6
+	 * @return  address or empty string
+	 */
+	public static String getIPAddress(boolean useIPv4) {
+		try {
+			List<NetworkInterface> interfaces = Collections.list(NetworkInterface.getNetworkInterfaces());
+			for (NetworkInterface intf : interfaces) {
+				List<InetAddress> addrs = Collections.list(intf.getInetAddresses());
+				for (InetAddress addr : addrs) {
+					if (!addr.isLoopbackAddress()) {
+						String sAddr = addr.getHostAddress();
+						//boolean isIPv4 = InetAddressUtils.isIPv4Address(sAddr);
+						boolean isIPv4 = sAddr.indexOf(':')<0;
+
+						if (useIPv4) {
+							if (isIPv4)
+								return sAddr;
+						} else {
+							if (!isIPv4) {
+								int delim = sAddr.indexOf('%'); // drop ip6 zone suffix
+								return delim<0 ? sAddr.toUpperCase() : sAddr.substring(0, delim).toUpperCase();
+							}
+						}
+					}
+				}
+			}
+		} catch (Exception ex) { } // for now eat exceptions
+		return "";
 	}
 }
