2d9baa8fbbc0312ae62e23d55550158f62776706
0xmatthias <matsek1000@web.de>
2017-03-08 21:22:21 +0100

    added explicit license

--- src/test/java/com/cryptolib/SecureDataSocketClientWLAN.java
+++ src/test/java/com/cryptolib/SecureDataSocketClientWLAN.java
@@ -1,3 +1,19 @@
+/*     This file is part of SecureDataSocket
+	   Copyright (C) 2017 Jakob Bode and Matthias Sekul
+
+	   SecureDataSocket is free software: you can redistribute it and/or modify
+	   it under the terms of the GNU General Public License as published by
+	   the Free Software Foundation, either version 3 of the License, or
+	   (at your option) any later version.
+
+	   SecureDataSocket is distributed in the hope that it will be useful,
+	   but WITHOUT ANY WARRANTY; without even the implied warranty of
+	   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	   GNU General Public License for more details.
+
+	   You should have received a copy of the GNU General Public License
+	   along with SecureDataSocket.  If not, see <http://www.gnu.org/licenses/>*/
+
 package com.cryptolib;
 
 import com.cryptolib.CryptoSocketInterface;
@@ -13,7 +29,7 @@ public class SecureDataSocketClientWLAN{
 			SecureDataSocket cs = new SecureDataSocket(4711);
 			System.out.println(cs.setupClientNoCamera("192.168.178.34:4711"));
 	                //Never do this without checking, if the phrases on both devices are the same. If they are different call cs.comparesPhrases(false)
-                        cs.comparedPhrases(true);
+						cs.comparedPhrases(true);
 
 			byte[] testb = cs.read();
 			System.out.println("Read!");
